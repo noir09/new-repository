3eb757a65322616cf50eb6d2b28546665c6eb410
Nikita <aldnor09@gmail.com>
2019-06-09 12:30:59 +0300

    new

---
+++ 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            string str;
+            Console.WriteLine("Введите число x");
+            str = Console.ReadLine();
+            x = Convert.ToInt32(str);
+            Console.WriteLine("Введите число y");
+            str = Console.ReadLine();
+            y = Convert.ToInt32(str);
+            if (x == y) Console.WriteLine("x= y");
+            if(x > y)
+            {
+                Console.WriteLine("x больше y на " +(x-y));
+            }
+            if (y > x) Console.WriteLine("y больше x на " + (y - x));
+        }
+    }
+}
---
+++ 6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //x=-0.6, D=0.05, k=20
+            //3*x-(1+1/x)ln(1-x*x)+2ln((1-x)/(1+x))
+            double x =-0.6;
+            double d = 0.05;
+            int k = 20;
+            double f;
+            Console.WriteLine("3*x-(1+1/x)ln(1-x*x)+2ln((1-x)/(1+x))");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i = 0; i < k; i++)
+            {
+                f = 3 * x - (1 + 1 / x) * Math.Log(1 - x * x) + 2 * Math.Log((1 - x) / (1 + x));
+                x += d;
+                Console.WriteLine("f1(x) = " + f);
+            }
+        }
+    }
+}
